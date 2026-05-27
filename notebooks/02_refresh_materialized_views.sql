d7621edd96a0d080f2ea495b3f3dc2ee7de65127
Eduardo <eduardororo285@gmail.com>
2026-05-27 14:49:11 -0500

    Adding databricks artifacts to bundle

---
+++ notebooks/02_refresh_materialized_views.sql
@@ -0,0 +1,9 @@
+REFRESH MATERIALIZED VIEW fintech_finpay.gold.dim_date;
+
+REFRESH MATERIALIZED VIEW fintech_finpay.gold.dim_user;
+
+REFRESH MATERIALIZED VIEW fintech_finpay.gold.dim_merchant;
+
+REFRESH MATERIALIZED VIEW fintech_finpay.gold.dim_channel;
+
+REFRESH MATERIALIZED VIEW fintech_finpay.gold.fact_transactions;
